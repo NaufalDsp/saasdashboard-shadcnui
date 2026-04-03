d155acb21010a295a3e4f56905ec0a5b2a64f308
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-03 23:44:03 +0700

    feat: implement AppSidebar component and integrate it into App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@
 
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/AppSidebar';
 
 export const App = () => {
   return (
     <ThemeProvider defaultTheme='dark'>
-      <div></div>
+      <SidebarProvider>
+        <AppSidebar />
+
+        <SidebarInset></SidebarInset>
+      </SidebarProvider>
     </ThemeProvider>
   );
 };
