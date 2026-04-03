f8ea0ede589306fa299b72d4be636fb89cd9376d
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-03 23:13:00 +0700

    feat: set default theme for ThemeProvider in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from './components/ThemeProvider';
 
 export const App = () => {
   return (
-    <ThemeProvider>
+    <ThemeProvider defaultTheme='dark'>
       <div></div>
     </ThemeProvider>
   );
