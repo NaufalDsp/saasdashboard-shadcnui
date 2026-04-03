6bb2f4279acb1fdddec7f550820fb064add0daba
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-03 23:52:12 +0700

    refactor: reorganize imports in AppSidebar component for better structure

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,3 +1,44 @@
+/**
+ * Custom Modules
+ */
+import { cn } from '@/lib/utils';
+
+/**
+ *
+ * Components
+ */
+import Avatar from 'react-avatar';
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarContent,
+  SidebarFooter,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarGroup,
+  SidebarGroupContent,
+} from '@/components/ui/sidebar';
+import { Button } from '@/components/ui/button';
+
+/**
+ *
+ * Hooks
+ */
+import { useSidebar } from '@/components/ui/sidebar';
+
+/**
+ *
+ * Assets
+ */
+import { LogOutIcon } from 'lucide-react';
+import { Logo } from '@/assets/Logo';
+
+/**
+ *
+ * Constants
+ */
+
 export const AppSidebar = () => {
   return <div className=''>App Sidebar</div>;
 };
