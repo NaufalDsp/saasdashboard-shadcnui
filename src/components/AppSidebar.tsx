3dae602d453c4ffd427368d10c3b6a6ba098f33a
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-04 15:10:18 +0700

    feat: enhance secondary navigation in AppSidebar and add user profile display in footer

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -84,27 +84,63 @@ export const AppSidebar = () => {
         </SidebarGroup>
 
         {/* Secondary Nav */}
-        <SidebarGroup className='mt-auto'>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {APP_SIDEBAR.secondaryNav.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton
-                    tooltip={item.title}
-                    asChild
-                  >
-                    <a href={item.url}>
-                      <item.Icon />
+        {isMobile && (
+          <SidebarGroup className='mt-auto'>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {APP_SIDEBAR.secondaryNav.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      tooltip={item.title}
+                      asChild
+                    >
+                      <a href={item.url}>
+                        <item.Icon />
 
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        )}
       </SidebarContent>
+
+      {/* Sidebar Footer */}
+      <SidebarFooter className={cn(isMobile && 'border-t')}>
+        <SidebarMenu>
+          <SidebarMenuItem className={cn(isMobile && 'p-2')}>
+            {isMobile ? (
+              <div className=''>
+                <div className='grid grid-cols-[max-content_minmax(0,1fr)] items-center gap-2'>
+                  <div className='relative'>
+                    <Avatar
+                      src={APP_SIDEBAR.curProfile.src}
+                      size='36px'
+                      round='8px'
+                    />
+
+                    <div className='absolute bottom-0 right-0 size-2 rounded-full bg-emerald-500 dark:bg-emerald-400 ring-sidebar ring-1'></div>
+                  </div>
+
+                  <div>
+                    <h3 className='text-sm font-semibold'>
+                      {APP_SIDEBAR.curProfile.name}
+                    </h3>
+                    <p className='text-sm text-muted-foreground truncate'>
+                      {APP_SIDEBAR.curProfile.email}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 };
