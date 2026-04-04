1140f8f0719dc332644b3b0f232608d32b427eb2
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-04 21:01:44 +0700

    feat: implement DropdownMenu component with various subcomponents for enhanced user interaction

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,3 +1,54 @@
+import Avatar from 'react-avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+
+import { PlusIcon } from 'lucide-react';
+
+import { APP_SIDEBAR } from '@/constants';
+
 export const UserMenu = () => {
-  return <div>User Menu </div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className='relative'>
+          <Avatar
+            src={APP_SIDEBAR.curProfile.src}
+            size='32px'
+            round='8px'
+          />
+
+          <div className='absolute bottom-0 right-0 size-2 rounded-full bg-emerald-500 dark:bg-emerald-400 ring-sidebar ring-1'></div>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        side='right'
+        align='end'
+        className='w-60'
+      >
+        <DropdownMenuGroup>
+          {APP_SIDEBAR.userMenu.itemsPrimary.map((item) => (
+            <DropdownMenuItem key={item.title}>
+              <item.Icon />
+
+              <span>{item.title}</span>
+              {item.kbd && (
+                <DropdownMenuShortcut>{item.kbd}</DropdownMenuShortcut>
+              )}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
