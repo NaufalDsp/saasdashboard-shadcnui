a4ad5260872327fa59cccdaa056d0775b39159bd
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-07 00:00:26 +0700

    feat: implement ThemeToggle component with dropdown menu for theme switching

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,3 +1,30 @@
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+import { useTheme } from '@/components/ThemeProvider';
+
+import { MoonIcon, SunIcon, MonitorIcon, CheckIcon } from 'lucide-react';
+
 export const ThemeToggle = () => {
-  return <div>Theme Toggle</div>;
+  const { Theme, setTheme } = useTheme;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='ghost'
+          size='icon'
+          aria-label='Toggle theme'
+        >
+          <SunIcon className='scale-100 rotate-0 tranisiton-all dark:scale-0 dark:-rotate-90' />
+
+          <MoonIcon className='absolute scale-100 rotate-90 tranisiton-all dark:scale-100 dark:rotate-0' />
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
