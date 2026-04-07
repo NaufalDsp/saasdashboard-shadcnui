c4e97d71d31551cf4aa284cc58524def13a0a926
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-07 20:00:03 +0700

    feat: update PageHeader component with enhanced layout and functionality

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -8,5 +8,37 @@ export const Page = ({ children }: React.PropsWithChildren) => {
 };
 
 export const PageHeader = () => {
-    return <div>Page Header</div>
-}
+  return (
+    <div className='flex flex-col gap-4 lg:flex-row lg:justify-between'>
+      <h1 className='text-xl font-semibold lg:text-2xl'>
+        Welcome back, Naufal
+      </h1>
+
+      <div className='flex gap-3'>
+        <div className=' flex max-lg:hidden'>
+          <ThemeToggle />
+          <Button
+            variant='ghost'
+            size='icon'
+            aria-label='Search'
+          >
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className='flex items-center gap-3'>
+          <Button variant='outline'>
+            <Settings2Icon />
+
+            <span>Customize</span>
+          </Button>
+          <Button variant='outline'>
+            <DownloadIcon />
+
+            <span>Export</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
