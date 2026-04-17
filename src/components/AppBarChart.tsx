17f43996c28a2f775eaa3167503dda0d9d7cf027
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-17 23:39:21 +0700

    feat: enhance AppBarChart with additional recharts components for improved visualization

--- src/components/AppBarChart.tsx
+++ src/components/AppBarChart.tsx
@@ -1,4 +1,10 @@
-import { Bar, BarChart } from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartLegend,
+  ChartTooltipContent,
+} from '@/components/ui/chart';
 
 export const AppBarChart = () => {
   return <div>s</div>;
