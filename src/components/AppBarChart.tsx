4c0e32fb8ce5d0cb92c2609939f871f916c883b0
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-04-18 21:03:11 +0700

    feat: implement AppBarChart component with responsive design and chart configuration

--- src/components/AppBarChart.tsx
+++ src/components/AppBarChart.tsx
@@ -6,6 +6,31 @@ import {
   ChartTooltipContent,
 } from '@/components/ui/chart';
 
+import { useIsMobile } from '@/hooks/use-mobile';
+
+import type { ChartConfig, ChartConfig } from '@/components/ui/chart';
+
+import { VENDOR_BREAKDOWN } from '@/constants';
+
+const ChartConfig = {
+  eu: { label: 'EU', color: 'var(--chart-1)' },
+  us: { label: 'US', color: 'var(--chart-2)' },
+  asia: { label: 'Asia', color: 'var(--chart-3)' },
+} satisfies ChartConfig;
+
 export const AppBarChart = () => {
-  return <div>s</div>;
+  const isMobile = useIsMobile();
+
+  return (
+    <ChartContainer
+      config={ChartConfig}
+      className=''
+    >
+      <BarChart accessibilityLayer>
+        <CartesianGrid vertical={false} />
+
+        <XAxis />
+      </BarChart>
+    </ChartContainer>
+  );
 };
